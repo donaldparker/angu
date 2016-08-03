b183cedf834c063980dbfe0b6f32582a5ac5e439
Donald Parker <DonaldParkerII@gmail.com>
2016-08-02 21:14:53 -0700

    configs, hello angular

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<div><h1>Hello Angular 2</h1><div>It has been a while</div></div>'
+})
+
+export class AppComponent { };
